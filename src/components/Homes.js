6022bcaae6aa107f209331c80c89c5da05505ccb
Stensis <irenenjuguna98@gmail.com>
2022-09-13 23:09:19 +0300

    added mit license

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../App.css'
 import { useState } from 'react'
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button'
+// import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button'
 
 function Homes() {
   // USE STATE
